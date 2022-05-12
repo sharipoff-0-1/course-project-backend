1d027748dbef2bc49339b6fc40962d35ff6010c9
sharipoff-0-1 <tedcodes.01@gmail.com>
2022-05-12 19:27:55 +0200

    make it better

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
+import userRouter from "./routes/user.js";
 
 const app = express();
 dotenv.config();
@@ -12,9 +13,8 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.use("/user", userRouter);
 
-// const CONNECTION_URL =
-//   "mongodb+srv://Ted:ted12345@collectiondb.ntn6l.mongodb.net/item?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 // Connect to Mongo
@@ -27,5 +27,3 @@ mongoose
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
   )
   .catch((err) => console.log(err));
-
-// mongoose.set("useFindAndModify", false);
