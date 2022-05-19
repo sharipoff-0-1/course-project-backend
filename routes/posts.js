e9aba6b8311bb5d0d1e72d27183f9c45f1f475aa
sharipoff-0-1 <tedcodes.01@gmail.com>
2022-05-19 22:05:14 +0200

    make it better

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,9 @@
 import express from "express";
 
 import {
+  getPostsBySearch,
   getPosts,
+  getPost,
   createPost,
   updatePost,
   deletePost,
@@ -12,7 +14,10 @@ import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
+router.get("/search", getPostsBySearch);
 router.get("/", getPosts);
+router.get("/:id", getPost);
+
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
